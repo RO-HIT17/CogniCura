5354c9f7b9a9b6e6019a3c9b48b27fa64b2356ff
vjk-2k5 <svijaykrishna12@gmail.com>
2024-10-25 17:32:09 +0530

    navigation pat

--- client/app/patdashboard/page.tsx
+++ client/app/patdashboard/page.tsx
@@ -7,15 +7,15 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 
-function DoctorDashboard() {
+function PatientDashboard() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
         <span className={title()}>Welcome to the </span>
-        <span className={title({ color: "violet" })}>Doctor Dashboard</span>
+        <span className={title({ color: "violet" })}>Patient Dashboard</span>
         <br />
         <span className={title()}>
-          Manage your appointments and patient history
+          Manage your appointments and medical history
         </span>
         <div className={subtitle({ class: "mt-4" })}>
           Easily view and manage your upcoming appointments and past treatments.
@@ -25,16 +25,16 @@ function DoctorDashboard() {
       <div className="flex gap-3">
         <NextLink
           className="inline-block"
-          href="/appointments" 
+          href="/patient-app-register" 
         >
           <Button auto color="primary" radius="full" variant="shadow">
-            View Upcoming Appointments
+            Register New Appointment
           </Button>
         </NextLink>
         
         <NextLink
           className="inline-block"
-          href="/history"
+          href="/patient-app-history"
         >
           <Button auto color="secondary" radius="full" variant="bordered">
             View Appointment History
@@ -46,4 +46,4 @@ function DoctorDashboard() {
   );
 }
 
-export default withAuth(DoctorDashboard);
+export default withAuth(PatientDashboard);
